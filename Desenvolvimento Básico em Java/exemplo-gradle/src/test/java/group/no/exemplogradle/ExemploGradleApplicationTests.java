1a708cfb605cd5639f7d529ff63789755fbcbf23
Paulo Henrique Leme <leme.pauloh@gmail.com>
2021-07-09 17:34:56 -0300

    aula 02 added

---
+++ Desenvolvimento Básico em Java/exemplo-gradle/src/test/java/group/no/exemplogradle/ExemploGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package group.no.exemplogradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Desenvolvimento Básico em Java/exemplo-maven/src/test/java/group/no/exemplomaven/ExemploMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package group.no.exemplomaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
